6d3136b7877cb51b3ec7d22d981e31e75f37f32e
vusaar <sourcecode@nust.ac.zw>
2023-08-13 16:42:27 +0200

    jjjf

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-     <React.StrictMode>
+     <BrowserRouter basename={process.env.PUBLIC_URL}>
        <App/>
-     </React.StrictMode>
+     </BrowserRouter>
     
  
 );
